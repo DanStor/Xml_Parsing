f6a95598453a8db6dd919782a73f6e0c5f8bdc95
Daniel Storey <dstorey@spartaglobal.com>
2018-10-16 16:22:31 +0100

    nokogiri commit

---
+++ lib/menu.rb
@@ -0,0 +1,30 @@
+require 'nokogiri'
+
+class Menu
+  attr_accessor :menu
+
+  def initialize xml_file
+    @menu = Nokogiri::XML(File.open(xml_file))
+  end
+
+  def get_food_names
+    # .search from Nokigiri
+    # Find all the name tags in the xml file
+    @menu.search('name')
+  end
+
+  def get_food_prices
+    # .search from Nokigiri
+    # Find all the price tags (haha) in the xml file
+    @menu.search('price')
+  end
+
+  def getXPathNames
+    @menu.xpath('//name')
+  end
+
+end
+
+bobsBurgers = Menu.new 'xml/menu.xml'
+# puts bobsBurgers.get_food_names
+puts bobsBurgers.getXPathNames
